c4d6a40e061b25e7e86d8c47bacdb674b841efee
Asurin <qiaozha@gmail.com>
2017-09-14 16:11:37 -0500

    flask config

--- src/web/__init__.py
+++ src/web/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 
+
 app = Flask(__name__)
-app.config.from_object('config')
+app.config.from_pyfile('config.py')
 bootstrap = Bootstrap(app)
 
 from web import views
